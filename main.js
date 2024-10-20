7a9d29c084afce6afca9e803e34669220e6f281b
tahsin <u1704102@student.cuet.ac.bd>
2024-10-21 02:36:45 +0600

    finally resolved cors

--- main.js
+++ main.js
@@ -24,12 +24,10 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const email = document.getElementById('loginUsername').value;
   const password = document.getElementById('loginPassword').value;
 
-  const response = await fetch('http://127.0.0.1:5000/auth/login', {
+  const response = await fetch('http://localhost:5000/auth/login', {
     method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      'Accept': '*',
-      'Access-Control-Origin': "*"
+      "Content-Type": "application/json"
    },
     body: JSON.stringify({ email, password })
   });
